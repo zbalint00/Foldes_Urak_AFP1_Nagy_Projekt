606e37f9f3f5d298fd412258042d0feeb2d915a5
DimoHUN <86042345+DimoHUN@users.noreply.github.com>
2021-12-09 13:02:12 +0100

    Profil oldal
    A profil oldal benne e-mail, jelszó, felhasználónév funkciókkal bővítve.

--- mnist/profile.php
+++ mnist/profile.php
@@ -1,10 +1,53 @@
+<?php
+session_start();
+$server = 'localhost'; 
+$username = 'root';
+$password = '';
+$database = 'adat';
+$kapcsolat = mysqli_connect($server, $username, $password, $database);
+$eredmeny = $kapcsolat->prepare('SELECT jelszo, email FROM felhasznalo WHERE id = ?');
+// Account ID alapján keressük meg a felhasználó adatait.
+$eredmeny->bind_param('i', $_SESSION['userid']);
+$eredmeny->execute();
+//Kirakjuk változóba az adott IDhez tartozó adatokat.
+$eredmeny->bind_result($jelszo, $email);
+$eredmeny->fetch();
+$eredmeny->close();
+?>
 <html>
-    <head>
-        <title>Profil</title>
-    </head>
-    <body>
-
-        <!-- PROFIL OLDAL (Linksch Balázs) -->
-
-    </body>
+	<head>
+		<meta charset="utf-8">
+		<title>Saját rofil</title>
+		<link href="menuformaz.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+                <h1>Felhasználó</h1>
+				<a href="profil.php"><i class="fas fa-user-circle"></i>Profil</a>
+				<a href="ki.php"><i class="fas fa-sign-out-alt"></i>Kijelentkezés</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Saját profil</h2>
+			<div>
+				<p>A profilod adatai:</p>
+				<table>
+					<tr>
+						<td>Felhasználóvén:</td>
+						<td><?=$_SESSION['user']?></td>
+					</tr>
+					<tr>
+						<td>Jelszó:</td>
+						<td><?=$jelszo?></td>
+					</tr>
+					<tr>
+						<td>Email:</td>
+						<td><?=$email?></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</body>
 </html>
