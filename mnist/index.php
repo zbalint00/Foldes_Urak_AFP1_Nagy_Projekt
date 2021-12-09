4868f005d099f46023851f2e0b7aa16c38b77095
mbalazs01 <92944165+mbalazs01@users.noreply.github.com>
2021-12-09 02:30:33 +0100

    Index.php-ról elérhetőve tettem az MNIST oldalt

--- mnist/index.php
+++ mnist/index.php
@@ -1,16 +1,33 @@
+<?php 
+    session_start();
+    //MNIST Logika - (Makai Balázs)
+    $_SESSION['kerdesekSzama'] = 10;
+    $_SESSION['jóMegoldások'] = 0;
+
+    // Ha nincs bejelentkezve
+        //header("Location: /Project/login.php")
+?>
+
 <html>
     <head>
         <title>Főoldal</title>
     </head>
     <body>
 
-        <!-- ha nem vagyunk bejelentkezve menjen a LOGIN oldalra (Linksch Balázs) -->  
+        <!-- LOGIN RENDSZER (Linksch Balázs) -->  
+    
+        <!-- MNIST LOGIKA - (Makai Balázs)-->
+        <a href="/Project/mnist.php">MNIST</a>
 
-        <!-- MNIST LOGIKA - HA FELHASZNÁLÓ BEVAN JELENTKEZVE (Makai Balázs)-->
+            <?php
+             if(!empty($_SESSION['elozoJoMegoldasok'])) {
+                  echo "<p>Legutolsó alkalommal ".$_SESSION['elozoJoMegoldasok']." jó megoldása volt</p>";
+                 }
+            ?> 
 
-        <!-- PROFIL OLDAL - HA FELHASZNÁLÓ BEVAN JELENTKEZVE (Linksch Balázs) -->
+        <!-- PROFIL OLDAL - (Linksch Balázs) -->
 
-        <!-- RANKLISTA KIÍRÁSA - HA FELHASZNÁLÓ BEVAN JELENTKEZVE (Galvács István) -->
+        <!-- RANKLISTA KIÍRÁSA - (Galvács István) -->
 
     </body>
 </html>
