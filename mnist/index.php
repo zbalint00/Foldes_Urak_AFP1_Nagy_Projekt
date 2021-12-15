f320c5b2bb69732f94dd1fbc9ab0f69d492c79e9
gpita220 <83598512+gpita220@users.noreply.github.com>
2021-12-15 19:05:24 +0100

    index.php fájl frissítése és a problémák javítása

--- mnist/index.php
+++ mnist/index.php
@@ -5,7 +5,9 @@
     $_SESSION['jóMegoldások'] = 0;
 
     // Ha nincs bejelentkezve
-        //header("Location: /Project/login.php")
+    if($_SESSION["userLoggedIn"] != true) {
+        header('Location: login.php');
+    }
 ?>
 
 <html>
@@ -13,11 +15,16 @@
         <title>Főoldal</title>
     </head>
     <body>
+    <nav class="navtop">
+			<div>
+                <h1>Felhasználó</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profil</a>
+				<a href="mnist.php"><i class="fas fa-user-circle"></i>MNIST</a>
+				<a href="ki.php"><i class="fas fa-sign-out-alt"></i>Kijelentkezés</a>
+			</div>
+		</nav>
 
-        <!-- LOGIN RENDSZER (Linksch Balázs) -->  
-    
         <!-- MNIST LOGIKA - (Makai Balázs)-->
-        <a href="/Project/mnist.php">MNIST</a>
 
             <?php
              if(!empty($_SESSION['elozoJoMegoldasok'])) {
@@ -25,9 +32,40 @@
                  }
             ?> 
 
-        <!-- PROFIL OLDAL - (Linksch Balázs) -->
-
         <!-- RANKLISTA KIÍRÁSA - (Galvács István) -->
+        <div id="rangsor">
+        <h1><u>Rangsor:</u></h1>
+        <?php
 
+        include("connection.php");
+        $db = new dbObj();
+        $connection = $db->getConnection();
+
+        
+        $query="SELECT felhasznalonev, pontszam FROM rangsor ORDER BY pontszam DESC";
+       
+        $response=array();
+        $result=mysqli_query($connection, $query);
+        $rank=1;
+        echo "<table border=1>";
+            echo "<tr>
+            <th> Rang</th>
+                <th>Felhasználónév</th>
+                <th>Pontszám</th>
+            </tr>";
+        while($row=mysqli_fetch_assoc($result))
+        { 
+            
+            echo"<tr>";
+            echo "<td width=20 align=center>". $rank .".". "</td>";
+            echo"<td width=100>" . $row['felhasznalonev'] . "</td>";
+            echo"<td wdith=100 align=center>" . $row['pontszam'] ." Pt" . "</td>";
+            echo "</tr>";
+            
+           
+         $rank++;
+        }  
+        echo "</table>";
+        ?>
     </body>
 </html>
